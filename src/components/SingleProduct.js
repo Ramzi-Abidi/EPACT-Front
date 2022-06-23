ee5ecc249b0fcf2117182358760d451747ef2841
Ramzi <ramziinfo2001@gmail.com>
2022-06-23 19:34:11 +0100

    scheduled commit

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -32,29 +32,34 @@ const SingleProduct = ({ prod, setProducts, products }) => {
   //let API_ENDPOINT = process.env.API_ENDPOINT;
 
   return (
-    <div>
+    <div style={{margin:"1rem"}}>
       <div className="card-product">
         <Link to={`/product/${prod._id}`}>
           <img className="medium" src={`http://102.219.178.49:5000/${prod.image}`} alt="product" />
         </Link>
         <div className="card-body">
-          <Link to={`/product/${prod._id}`}>
-            <h2 style={{ padding: "0.5rem 0", marginTop: "0.85rem", color: "#333" }}> Nom : {prod.name} </h2>
-          </Link>
-          <div className="price">Prix : {prod.price} dt</div>
+          <div style={{margin: "0 2.5rem"}}>
+            <div>
+              <Link to={`/product/${prod._id}`}>
+                <h2 style={{ padding: "0.5rem 0", marginTop: "0.85rem", color: "#333" }}> {prod.name} </h2>
+              </Link>
+            </div>
+            <div className="price">{prod.price} dt</div>
 
           {localStorage.getItem("userInfo") && JSON.parse(localStorage.getItem("userInfo")).isAdmin &&
             <div>
-            
+
 
               <div className="share">
-                <div style={{display:"flex",justifyContent:"center"}}>
-                  <Link to={`editProduct/${prod._id}`} style={{ color: "#333", fontWeight: "bold" }} className="edit"> <Button variant="outline-success" style={{ padding: ".6rem 3rem",fontWeight:"bold",fontSize:"1.1rem" }} className="editButton">Editer</Button> </Link>  {' '}
-                  <Button variant="outline-danger" style={{ padding: ".6rem 1.8rem", fontWeight: "bold",fontSize:"1.1rem",marginLeft:"1.5rem" }} onClick={() => handleDelete(prod._id)}>Supprimer</Button>{' '}
+                <div >
+                  <Link to={`editProduct/${prod._id}`} style={{ color: "#333", fontWeight: "bold" }} className="edit"> <Button variant="outline-success" style={{ padding: ".6rem 3rem", fontWeight: "bold", fontSize: "1.1rem" }} className="editButton">Editer</Button> </Link>  {' '}
+                  <Button variant="outline-danger" style={{ padding: ".6rem 1.8rem", fontWeight: "bold", fontSize: "1.1rem", marginLeft: "1.5rem" }} onClick={() => handleDelete(prod._id)}>Supprimer</Button>{' '}
                 </div>
               </div>
             </div>
           }
+          </div>
+
         </div>
       </div>
     </div>
